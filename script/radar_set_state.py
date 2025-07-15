5849ddec27e896694c207102a88ffcf303731671
seungjae99 <seungjaechoi9@gmail.com>
2025-07-15 15:57:37 +0900

    add standby state node

---
+++ script/radar_set_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import rospy
+from marine_radar_control_msgs.msg import RadarControlValue
+
+def main():
+    rospy.init_node('radar_set_state')
+    state = rospy.get_param('~state', 'standby')          # 기본 standby
+    topic = rospy.get_param('~topic', '/HaloA/change_state')
+
+    pub = rospy.Publisher(topic, RadarControlValue, queue_size=1, latch=True)
+    msg = RadarControlValue()
+    msg.key = "status"
+    msg.value = state
+
+    # 0.5 s 동안 5 회 정도 반복 송신 후 종료
+    rate = rospy.Rate(10)
+    for _ in range(5):
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    main()
